f95002f492709bbef8d57b6c1ffed5cccc48025a
fadimata tillman <fadi.tillman@gmail.com>
2020-04-21 17:06:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,20 +3,20 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 
-  # get '/My name is /:name' do |n|
+   get '/My name is /:name' do |n|
   #  matches "GET /My name is /foo" and "GET /My name is /bar"
   #  params['name'] is 'foo' or 'bar'
   #  n stores params['name']
-  # "My name is  #{n}!"
+  "My name is  #{n}!"
 
 
 
-   get '/name' do
-  @name = Name.all
-
-  erb :'name/index.html.erb'
- "My name is #{name}"
-end
+#    get '/name' do
+#   @name = Name.all
+# 
+#   erb :'name/index.html.erb'
+#  "My name is #{name}"
+# end
 
 
 get '/hometown' do
