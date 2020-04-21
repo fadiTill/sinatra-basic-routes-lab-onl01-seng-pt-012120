7f2ca3144efc3ceeceb8a6df5db999da4eeb0b2b
fadimata tillman <fadi.tillman@gmail.com>
2020-04-21 16:19:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
+  # describe "Basic Routes Lab" do
+  #   describe "GET '/name'" do
+  #     before(:each) do
+  #       get '/name'
+  #     end
+  # 
+  #     it "returns a 200 status code" do
+  #       expect(last_response.status).to eq(200)
+  #     end
+  # 
+  #     it "displays your name" do
+  #       expect(last_response.body).to include("My name is ")
+  #     end
+  #   end
+
   def name
 get '/name' do
 @name = Name.all
