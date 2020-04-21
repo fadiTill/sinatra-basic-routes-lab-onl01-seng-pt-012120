79477585dc4d8180c215a9fb834704750f4f9b56
fadimata tillman <fadi.tillman@gmail.com>
2020-04-21 16:17:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  def name
+get '/name' do
+@name = Name.all
+"My name is"
+erb :'name/index.html.erb'
+end
+
+get '/hometown' do
+
+"My hometown is #{hometown}"
+
+end
+
+get '/favorite-song' do
+
+"My favorite song is#{favorite-song}"
+
+end
 end
